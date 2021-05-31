9c78c7932ade11676ba0b6491da544b2a7a20a9f
ahanna334 <78327872+ahanna334@users.noreply.github.com>
2021-05-31 15:20:41 -0700

    Update crop-image-pure.js

--- crop-image-pure.js
+++ crop-image-pure.js
@@ -1,23 +1,23 @@
 /*global document:false, window:false*/
-!function(window) {
-	function resizeableImage(image_target) {
-    	'user strict';
+!function (window) {
+    function resizeableImage(image_target) {
+        'user strict';
 
         var cropComponent
-                , container
-                , crop_img
-                , event_state = {}
-                , ratio = 1.0
-                , keyZoomValue = 4.0
-                , MINWIDTH = 50
-                , MINHEIGHT = 50
-                , CROPWIDTH = 200
-                , CROPHEIGHT = 200
-                , cropLeft = 0
-                , cropTop = 0
-                , cropWidth = 0
-                , cropHeight = 0
-                , resize_canvas = null;
+            , container
+            , crop_img
+            , event_state = {}
+            , ratio = 1.0
+            , keyZoomValue = 4.0
+            , MINWIDTH = 50
+            , MINHEIGHT = 50
+            , CROPWIDTH = 200
+            , CROPHEIGHT = 200
+            , cropLeft = 0
+            , cropTop = 0
+            , cropWidth = 0
+            , cropHeight = 0
+            , resize_canvas = null;
 
         if (image_target.complete) {
             init();
@@ -45,7 +45,7 @@
             container.addEventListener('wheel', resizing, false);
 
             document.addEventListener('keypress', keyHandler, false);
-            document.querySelector('.js-crop').addEventListener('click', openCropCanvasImg);
+            document.querySelector('.btn-crop').addEventListener('click', openCropCanvasImg);
         }
 
         function init() {
@@ -60,6 +60,7 @@
             image_target.draggable = false;
 
             crop_img = new Image();
+            crop_img.crossOrigin = image_target.crossOrigin;
             crop_img.src = image_target.src;
             crop_img.draggable = false;
 
@@ -77,7 +78,6 @@
             cropComponent.appendChild(crop_img);
             cropComponent.appendChild(image_target);
             container.appendChild(crop_img);
-            //используйте картинку из своего домена. иначе получите ошибку No 'Access-Control-Allow-Origin' header is present on the requested resource.
 
             left = image_target.offsetWidth / 2 - CROPWIDTH / 2;
             top = image_target.offsetHeight / 2 - CROPHEIGHT / 2;
@@ -122,14 +122,15 @@
         }
 
         function imgZoom(zoom) {
+            zoom = zoom * Math.PI * 2
             var newWidth = Math.floor(container.clientWidth + zoom)
-                    , newHeight = Math.floor(container.clientHeight + zoom)
-                    , w = crop_img.clientWidth
-                    , h = crop_img.clientHeight
-                    , left
-                    , top
-                    , right
-                    , bottom;
+                , newHeight = Math.floor(container.clientHeight + zoom)
+                , w = crop_img.clientWidth
+                , h = crop_img.clientHeight
+                , left
+                , top
+                , right
+                , bottom;
 
             if (newWidth < MINWIDTH) {
                 return;
@@ -167,10 +168,10 @@
             e.preventDefault();
 
             switch (String.fromCharCode(e.charCode)) {
-                case '+' :
+                case '+':
                     imgZoom(keyZoomValue);
                     break;
-                case '-' :
+                case '-':
                     imgZoom(-keyZoomValue);
                     break;
             }
@@ -178,8 +179,7 @@
 
         function resizing(e) {
             e.preventDefault();
-
-            imgZoom(e.deltaY);
+            imgZoom(e.deltaY > 0 ? 1 : -1);
         }
 
         function startMoving(e) {
@@ -205,10 +205,10 @@
 
         function moving(e) {
             var curuntTouch = {}
-                    , left
-                    , top
-                    , w
-                    , h;
+                , left
+                , top
+                , w
+                , h;
 
             e.preventDefault();
             e.stopPropagation();
@@ -237,7 +237,6 @@
         }
 
         function crop() {
-					  crop_img.crossOrigin = 'anonymous';
             cropWidth = crop_img.width * ratio;
             cropHeight = crop_img.height * ratio;
 
@@ -246,22 +245,24 @@
 
             var ctx = resize_canvas.getContext('2d');
             ctx.drawImage(crop_img,
-                    cropLeft, cropTop,
-                    cropWidth, cropHeight
+                cropLeft, cropTop,
+                cropWidth, cropHeight
             );
         }
 
         function openCropCanvasImg() {
-          crop();
-					
-					try {
-						var base64Img = resize_canvas.toDataURL("image/png", 1.0);
-            window.open(base64Img);
-					} catch(e) {
-						alert(e);
-					} finally {
-						removeHandlers();
-					}
+            crop();
+
+            try {
+                var base64Img =
+                    resize_canvas.toDataURL("image/png");
+                var newTab = window.open('about:blank', 'image from canvas');
+                    newTab.document.write("<img src='" + base64Img + "' alt='from canvas'/>");
+            } catch (e) {
+                alert(e);
+            } finally {
+                // removeHandlers();
+            }
 
         }
     }
